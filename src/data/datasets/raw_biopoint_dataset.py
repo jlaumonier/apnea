267e7e3a34c1e05913810e00520eb86926a067d5
Julien Laumonier <julien.laumonier@iid.ulaval.ca>
2023-07-25 11:31:51 -0400

    gestion des données du Biopoint : Temp en UTC/index, resampling a 40Hz, Remplacement des NaN par la derniere valeur

--- src/data/datasets/raw_biopoint_dataset.py
+++ src/data/datasets/raw_biopoint_dataset.py
@@ -1,8 +1,9 @@
 from os import listdir
 from os.path import isdir, isfile, join
-import re
+import datetime as dt
 
 import numpy as np
+import pytz
 import pandas as pd
 from numpy import genfromtxt
 from pyapnea.oscar.oscar_constants import ChannelID
@@ -16,7 +17,13 @@ from src.data.preparation_tasks import generate_annotations
 # TODO : need sliding window : https://discuss.pytorch.org/t/is-there-a-data-datasets-way-to-use-a-sliding-window-over-time-series-data/115702/4
 class RawBioPointDataset(Dataset):
 
-    def __init__(self, input_type='csv', output_type='numpy', limits=None, channels=None):
+    ORIG_FREC = {'ECG': 500,
+             'EMG': 2000,
+             'BioImp': 40,
+             'IMU': 50,
+             'TEMPERATURE': 1}
+
+    def __init__(self, input_type='csv', output_type='numpy', limits=None, channels=None, resampling_freq=None):
         """
 
         :param output_type: 'numpy' or 'dataframe'
@@ -32,37 +39,60 @@ class RawBioPointDataset(Dataset):
 
         self.channels = channels
 
+        self.dest_resampling_freq = resampling_freq
+
     def __len__(self):
         return len(self.list_files)
 
     # https://medium.com/analytics-vidhya/optimized-ways-to-read-large-csvs-in-python-ab2b36a7914e
     def __getitem__(self, idx):
-        def get_channel_from_filename(filename: str):
+        def get_info_from_filename(filename: str):
             filename = filename.replace('.', '_')
             elements = filename.split('_')
-            return elements[-2]
-
+            start_time = elements[-6]+'.'+elements[-5]+'.'+elements[-4]+' '+elements[-3]
+            return elements[-2], start_time
 
         result = None
         fullpath = self.list_files[idx]['fullpath']
-        files = [join(fullpath, f) for f in listdir(fullpath) if isfile(join(fullpath, f))]
+        files = [{'fullpath': join(fullpath, f), 'filename':f} for f in listdir(fullpath) if isfile(join(fullpath, f))]
         # TODO move to a specific boipoint util file
         global_df = pd.DataFrame(columns=['no_channel'])
         for f in files:
-            channel = get_channel_from_filename(f)
+            channel, start_time = get_info_from_filename(f['filename'])
+            start_time_timestamp = dt.datetime.strptime(start_time, "%Y.%m.%d %Hh%Mm%S")
+            tz = pytz.timezone("Canada/Eastern")
+            start_time_timestamp = tz.localize(start_time_timestamp, is_dst=None)
+            start_time_timestamp_utc = start_time_timestamp.astimezone(pytz.utc)
             if (channel is None) or (channel in self.channels):
-                print(channel)
+                print(channel, start_time_timestamp)
 
                 if self.output_type == 'dataframe':
                     try:
-                        chunk = pd.read_csv(f, chunksize=1000000)
+                        chunk = pd.read_csv(f['fullpath'], chunksize=1000000)
                         df = pd.concat(chunk)
+
+                        # recalculate the time column
+                        df['time_utc'] = (pd.date_range(start=start_time_timestamp_utc,
+                                                        periods=len(df),
+                                                        freq=str(1 / self.ORIG_FREC[channel]) + 'S'))
+
+                        df.set_index('time_utc', inplace=True)
+                        df.sort_index(inplace=True)
+
+                        # resampling
+                        if self.dest_resampling_freq:
+                            df = df.resample(str(1 / self.dest_resampling_freq) + 'S').mean()
+
                         if global_df.empty:
                             global_df = df
                         else:
-                            global_df = pd.merge(global_df, df, on='Time', how='outer', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
+                            global_df = pd.merge(global_df, df, left_index=True, right_index=True, how='outer', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
                     except pd.errors.EmptyDataError:
                         df = pd.DataFrame()
+
+        global_df.sort_index(inplace=True)
+        global_df.drop('Time', axis=1, inplace=True)
+        global_df.fillna(method='ffill', inplace=True)
 
         if self.output_type == 'numpy':
             result = None
--- src/display_biopoint.py
+++ src/display_biopoint.py
@@ -1,28 +1,29 @@
 import hydra
+import pandas as pd
 import matplotlib.pyplot as plt
 from codecarbon import EmissionsTracker  # see https://github.com/mlco2/codecarbon/issues/244
 
 from src.data.datasets.raw_biopoint_dataset import RawBioPointDataset
 
-
+pd.set_option('display.max_columns', None)
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(conf):
-    biopoint_ds = RawBioPointDataset(output_type='dataframe', channels=['BioImp', 'IMU', 'TEMPERATURE'])
-    # impossible de fusionner ECG avec les autres
-    biopoint_ds2 = RawBioPointDataset(output_type='dataframe', channels=['ECG'])
-    biopoint_ds3 = RawBioPointDataset(output_type='dataframe', channels=['EMG'])
+
+
+    biopoint_ds = RawBioPointDataset(output_type='dataframe',
+                                     channels=['BioImp', 'IMU', 'TEMPERATURE', 'ECG', 'EMG'],
+                                     resampling_freq=40)
     data = biopoint_ds[0]
-    data2 = biopoint_ds2[0]
-    data3 = biopoint_ds3[0]
-    print(data3)
+    print(data.head(20))
 
     plt.figure(figsize=(20, 10))
-    fig, (ax1, ax2, ax3) = plt.subplots(3)
-    ax1.plot(data['Time'], data['BIO_IMPEDANCE'])
-    ax2.plot(data2['Time'], data2['ECG'])
-    ax3.plot(data3['EMG'])
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+    ax1.plot(data.index, data['BIO_IMPEDANCE'])
+    ax2.plot(data.index, data['ECG'])
+    ax3.plot(data.index, data['EMG'])
+    ax4.plot(data.index, data['Temperature'])
     plt.show()
 
 if __name__ == "__main__":
-    with EmissionsTracker(output_dir='..') as tracker:
+    with EmissionsTracker(output_dir='..', log_level='error') as tracker:
         main()
