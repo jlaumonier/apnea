3b023a4498dca22e6264c374dab85d0fe2c55b36
Julien Laumonier <julien.laumonier@iid.ulaval.ca>
2023-09-11 15:23:26 -0400

    Refactoring LSTM model (in progress)

--- src/models/basic_lstm_model.py
+++ src/models/basic_lstm_model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence, pad_sequence
 from torch.nn import LogSoftmax
 import torch
 
@@ -8,32 +8,36 @@ import torch
 class BasicLSTMModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=100, num_layers=10, batch_first=True)
+        self.lstm = nn.LSTM(input_size=1, hidden_size=100, num_layers=1, batch_first=True)
         self.linear = nn.Linear(100, 1)
         self.sftmax = LogSoftmax(dim=2)
+        self.padding_value = -100.0
 
     def forward(self, input_):
-        # todo packed_pad_seq au lieu du collator
-
         print(input_.shape)
 
-        l = [(torch.Tensor(embeded_sequence), len(embeded_sequence)) for embeded_sequence in input_]
-        print(l)
-        input_tensor, lengths = zip(*l)
+        lengths = [len(embeded_sequence) for embeded_sequence in input_]
+        lengths = torch.Tensor(lengths)
+        print(lengths.shape)
 
-        print(input_tensor)
+        input_tensor = pad_sequence(input_,
+                                    batch_first=True,
+                                    padding_value=self.padding_value)
 
-        packed_input_tensor = pack_padded_sequence(input_tensor, batch_first=True,
+        print(input_tensor.shape)
+
+        packed_input_tensor = pack_padded_sequence(input=input_tensor,
+                                                   batch_first=True,
                                                    enforce_sorted=False,
-                                                   lengths=lengths)
-
-        #print(packed_input_tensor.shape)
+                                                   lengths=lengths.cpu())
 
         lstm_output, _ = self.lstm(packed_input_tensor)
 
         print(lstm_output.shape)
 
-        lstm_out, _ = pad_packed_sequence(lstm_output, batch_first=True, padding_value=-100.0)
+        lstm_out, _ = pad_packed_sequence(lstm_output,
+                                          batch_first=True,
+                                          padding_value=self.padding_value)
 
         print(lstm_out.shape)
 
