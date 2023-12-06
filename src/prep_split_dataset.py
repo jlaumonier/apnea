3304df439fd4346463ed8e93b2d26e24e913146e
Julien Laumonier <julien.laumonier@iid.ulaval.ca>
2023-12-06 16:24:01 -0500

    Tasks : windowed, pickle, split, train. Bugs remains, make tests

--- src/prep_split_dataset.py
+++ src/prep_split_dataset.py
@@ -1,19 +1,16 @@
+import os
 import hydra
 from codecarbon import EmissionsTracker  # see https://github.com/mlco2/codecarbon/issues/244
-import numpy as np
 
-from src.data.datasets.pickle_dataset import PickleDataset
-from src.data.preparation_tasks import split_dataset
+from src.pipeline.task import Task
 
-
-@hydra.main(config_path="../conf", config_name="config", version_base=None)
+@hydra.main(config_path="../conf", config_name="data-pipeline-split", version_base=None)
 def main(conf):
-    processed_dataset_complet = PickleDataset(output_type='numpy', src_data_path='../data/processing/pickle/')
-    split_dataset(processed_dataset_complet,
-                  output_dir_path='../data/processing/split',
-                  train_ratio=conf['training']['split']['train_ratio'],
-                  valid_ratio=conf['training']['split']['valid_ratio']
-                  )
+    conf.pipeline.data.dataset.source = 'dd337efa-189e-484a-b1bd-6f061a2fd022'
+
+    data_repo_path = os.path.join('..', 'data', 'repository')
+    simple_task = Task(data_repo_path, conf)
+    simple_task.run(conf)
 
 if __name__ == "__main__":
     with EmissionsTracker(output_dir='..', log_level='error') as tracker:
